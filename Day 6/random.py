3cf3b3a9ab8a6e26bfbbc54c8190b94984a3b59a
KHALIFAH334 <abdulhameedkhalifah@gmail.com>
2026-07-10 20:20:03 +0100

    Day 5

---
+++ Day 6/random.py
@@ -0,0 +1,11 @@
+#This is a python script that gives an execution grade depending on the rules followed during the execution of the trade
+from risk_engine import lot
+print("Welcome to Khalifah's trading system")
+def trade_execution():
+    a = int(input('Date: '))
+    b = input('Asset Traded: ')
+    c = input('Set-up Id: ')
+    d = input('HTF Bias: ')
+    e = int(input('Planned risk: '))
+    f = lot 
+    print(f"Today is {a}, {b}, the {c}, the {d}, the {e}, you should use {lot} ")
